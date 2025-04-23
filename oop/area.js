5877207fcd2950745d43fc9b899be1543a3d92da
dominam036 <dominik.namenyi@gmail.com>
2025-04-23 11:21:05 +0200

    tablazat letrehozasa oop

--- oop/area.js
+++ oop/area.js
@@ -1,4 +1,19 @@
 class Area {//létrehozunk egy osztályt
+    /**
+     * @param {HTMLDivElement}
+     */
+    #div; //privát változó amit csak osztályon belül látni
+
+    /**
+     * @returns {HTMLDivElement}
+     */
+    get div(){// getter metódus hogy el lehessen érni a privát div-et
+        return this.#div;// visszaadja a #div értékét
+    }
+    
+    /**
+     * @param {string} className - amit létre szeretnénk hozni pl 'table'
+     */
     constructor(className) { // konstruktor, kap egy class nevet
         let containsDiv = document.querySelector('.containeroop'); // megnézi van-e már ilyen nevű container
 
@@ -7,9 +22,38 @@ class Area {//létrehozunk egy osztályt
             containsDiv.className = 'containeroop'; // beállítja a class nevét
             document.body.appendChild(containsDiv); // hozzáadja a bodyhoz
         }
+
+        this.#div = document.createElement('div'); // létrehoz egy új divet a kapott class-hoz
+        this.#div.className = className; // beállítja a class nevet amit paraméterként kapott
+        containsDiv.appendChild(this.#div); // berakja az új divet a containeroop divbe
+    }
+}
+
+class Table extends Area { // létrehozunk egy Table nevű osztályt, ami az Area ősosztályból öröklődik
+    /**
+     * 
+     * @param {string} cssClass megadjuk kívülről amit kreálni szeretnénk vele
+     */
+    constructor(cssClass){ // konstruktor, kap egy css class nevet
+        super(cssClass); // meghívjuk az Area osztály konstruktorát vele
         
-        const div = document.createElement('div'); // létrehoz egy új divet a kapott class-hoz
-        div.className = className; // beállítja a class nevet amit paraméterként kapott
-        containsDiv.appendChild(div); // berakja az új divet a containeroop divbe
+        const table = document.createElement('table'); // csinálunk egy table elemet
+        this.div.appendChild(table); // hozzáadjuk a divhez amit az Area hozott létre
+
+        const thead = document.createElement('thead'); // létrehozzuk a táblázat fejléce részét
+        table.appendChild(thead); // hozzáadjuk a table-hez
+
+        const theadRow = document.createElement('tr'); // létrehozunk egy sort a fejléchez
+        thead.appendChild(theadRow); // hozzáadjuk a sort a thead-be
+
+        const fejlecNevek = ['Szerző', 'műfaj', 'cím']; // fejléc mezők szövegeit tároljuk itt egy tömbben
+        for(const fejlec of fejlecNevek){ // végigmegyünk a tömb elemein
+            const theadCella = document.createElement('th'); // csinálunk egy th elemet
+            theadCella.innerText = fejlec; // beleírjuk a szöveget
+            theadRow.appendChild(theadCella); // hozzáadjuk a sort a fejlécbe
+        }
+        
+        const tbody = document.createElement('tbody'); // csinálunk egy üres tbody részt is
+        table.appendChild(tbody); // hozzáadjuk a table-hez
     }
 }
--- oop/main.js
+++ oop/main.js
@@ -1,5 +1,5 @@
 const elvalaszto = document.createElement('hr'); // hogy a html-en egyszeruen megtalalhato legyen az elvalaszto oop es sima kozott
  document.body.appendChild(elvalaszto); // hozzáadjk a bodyhoz az elválasztó vonalat
  
- const table = new Area('table'); // létrehoz egy új 'table' típusú Area objektumot
- const form = new Area('form'); // ugyanaz mint fent, csak 'form' névvel
+ const table = new Table('table'); // létrehoz egy új Table példányt
+ const form = new Area('form'); // létrehoz egy új 'form' típusú Area példányt
