fa0383b38be409fd7af7e5dd0f2e48c64b242e8c
dominam036 <dominik.namenyi@gmail.com>
2025-04-23 11:27:27 +0200

    form letrehozasa oop

--- oop/area.js
+++ oop/area.js
@@ -57,3 +57,40 @@ class Table extends Area { // létrehozunk egy Table nevű osztályt, ami az Are
         table.appendChild(tbody); // hozzáadjuk a table-hez
     }
 }
+
+class Form extends Area { // Form nevű osztály, amely az Area osztályból öröklődik
+    /**
+     * @param {string} cssClass 
+     */
+    constructor(cssClass) { // konstruktor, megkapja a css osztály nevét
+        super(cssClass); // meghívja az Area osztály konstruktorát a cssClass paraméterrel
+
+        const urlapElem = document.createElement('form'); // létrehozunk egy <form> HTML elemet
+        this.div.appendChild(urlapElem); // a létrehozott form-ot hozzáadjuk a div-hez, amit az Area osztályban hoztunk létre
+
+        const mezoLista = [ // egy tömb, benne objektumokkal
+            { fieldid: 'szerzo', fieldLabel: 'Szerző' }, // elso mezo id, felirat
+            { fieldid: 'mufaj', fieldLabel: 'Műfaj' }, // masodik mezo id, felirat
+            { fieldid: 'cim', fieldLabel: 'cím' } // harmadik mezo id, felirat
+        ];
+
+        for (const mezoObjektum of mezoLista) { // végigmegyünk minden mező objektumon a tömbben
+            const mezoDiv = makeDiv('field'); // létrehozunk egy div-et a 'field' class névvel 
+            urlapElem.appendChild(mezoDiv); // a div-et hozzáadjuk az űrlaphoz
+
+            const label = document.createElement('label'); // létrehozunk egy label elemet
+            label.htmlFor = mezoObjektum.fieldid; // beállítjuk, hogy melyik inputhoz tartozik ez a label 
+            label.textContent = mezoObjektum.fieldLabel; // beállítjuk a label szövegét
+            mezoDiv.appendChild(label); // a label-t hozzáadjuk a div-hez
+
+            const input = document.createElement('input'); // létrehozunk egy input mezőt
+            input.id = mezoObjektum.fieldid; // beállítjuk az input mező id-jét
+            mezoDiv.appendChild(document.createElement('br')); // beszúrunk egy sortörést a label és az input közé
+            mezoDiv.appendChild(input); // hozzáadjuk az input mezőt is a div-hez
+        }
+
+        const hozzaadasGomb = document.createElement('button'); // létrehozunk egy button gomb elemet
+        hozzaadasGomb.textContent = 'hozzáadás'; // beállítjuk a gomb feliratát
+        urlapElem.appendChild(hozzaadasGomb); // a gombot hozzáadjuk az űrlaphoz
+    }
+}
--- oop/main.js
+++ oop/main.js
@@ -1,5 +1,5 @@
 const elvalaszto = document.createElement('hr'); // hogy a html-en egyszeruen megtalalhato legyen az elvalaszto oop es sima kozott
- document.body.appendChild(elvalaszto); // hozzáadjk a bodyhoz az elválasztó vonalat
+document.body.appendChild(elvalaszto); // hozzáadjk a bodyhoz az elválasztó vonalat
  
- const table = new Table('table'); // létrehoz egy új Table példányt
- const form = new Area('form'); // létrehoz egy új 'form' típusú Area példányt
+const table = new Table('table'); // létrehoz egy új Table példányt
+const form = new Form('form'); // létrehoz egy új Form példányt
